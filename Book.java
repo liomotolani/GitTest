57e340b84254987bfa404fdbfc6bff31a1b8094c
unknown <ligaliomotolani@gmail.com>
2019-05-14 15:06:52 +0100

    Book file

--- Book.java
+++ Book.java
@@ -1,4 +1,13 @@
 
 public class Book{
-	private int noOfPages;
+	private String author;
+	private String genre;
+	private String title;
+	private int numberOfPages;
+	private boolean isHardCover;
+
+	
+	public static void main(String[] args){
+	
+	}
 }
